6e2c9cc149d0c5c6aaa72df1d81ec5e672280907
Ramunas <rambek@gmail.com>
2019-10-24 12:54:00 +0300

    Update Hw_10_22.java

--- src/lt/bit/oop/home_works/Hw_10_22.java
+++ src/lt/bit/oop/home_works/Hw_10_22.java
@@ -13,7 +13,7 @@ class Hw_10_22 {
             if (lng == 5) break;
             }
         }
-        System.out.println("Skaičių nuo 1 iki 100 " + lng + " mažiausių skaičių suma lygi: " + suma);
+        System.out.println("Skaičių nuo 1 iki 100 " + lng + " mažiausių nelyginių skaičių suma lygi: " + suma);
 
         lng = 0;
         suma = 0;
@@ -23,5 +23,23 @@ class Hw_10_22 {
             if (lng == 3) break;
         }
         System.out.println("Skaičių nuo 1 iki 100 " + lng + " didžiausių skaičių suma lygi: " + suma);
+
+        int[] massA = {1, 2, 3, 4};
+        int[] massB = {5, 6, 7, 8};
+        int lenght = massA.length + massB.length;
+        int[] massA_B = new int[lenght];
+        int i = 0;
+            for (int element: massA) {
+                massA_B[i] = element;
+                i++;
+            }
+            for (int element: massB) {
+                massA_B[i] = element;
+                i++;
+            }
+        System.out.println("Pirmasis masyvas: " + Arrays.toString(massA));
+        System.out.println("Antrasis masyvas: " + Arrays.toString(massB));
+        System.out.println("Sujungti masyvai: " + Arrays.toString(massA_B));
+
     }
 }
